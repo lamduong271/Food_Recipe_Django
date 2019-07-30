3d90113ab69820596457f6e12f00ece1ad28c396
Lam Duong <lamdu@Lams-MacBook-Pro.local>
2019-07-30 14:28:29 +0300

    test authenticate and bla bla

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,16 +16,25 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self,instance, validated_data):
+        """Update an user, setting the password correctly and return it """
+        password = validated_data.pop('password', None) 
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object"""
     email = serializers.CharField()
     password = serializers.CharField(
-        style={'input_type':'password'},
-        trim_whitespace = False
+        style={'input_type': 'password'},
+        trim_whitespace=False
     )
     def validate(self, attrs):
         """Validate and authenticate the user"""
-        email = attrs.get('email'),
+        email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
             request=self.context.get('request'),
@@ -33,9 +42,9 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password
         )
         if not user:
-            msg = ('unable to authenticate with provided credentials')
+            msg = _('unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
-        return att
+        return attrs
 
