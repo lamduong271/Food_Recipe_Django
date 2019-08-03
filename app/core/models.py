e29691c69ce248f680dc25f8a772f145041abf62
Lam Duong <lamdu@Lams-MacBook-Pro.local>
2019-08-04 00:52:59 +0300

    image upload

--- app/core/models.py
+++ app/core/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.conf import settings
 # Create your models here.
 
+import uuid
+import os
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
+
+image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
@@ -58,5 +70,6 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to = recipe_image_file_path)
     def __str__(self):
         return self.title
